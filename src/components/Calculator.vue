a95db51f144f8d3f2602af3b3c90f70006a6fbbb
TheLegendWeeb <sebibucur2002@gmail.com>
2019-08-05 18:16:43 +0300

    Started the app

---
+++ src/components/Calculator.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Desired acceleration:&emsp;&emsp;&emsp;<input v-model="des_accel">   <span style="color:red" v-if="isNaN(des_accel)">&emsp; GET THE FUCK OUT</span> </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Calculator",
+    data(){
+        return{
+            des_accel:null
+        }
+    }
+}
+</script>
